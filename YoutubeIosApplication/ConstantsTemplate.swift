8c0821e5e9197df8e52e20ed8aa2e5d52292d94e
shivsupekar <shivsupekar123@gmail.com>
2024-08-12 10:23:37 +0530

    making changes in constants.swift

--- YoutubeIosApplication/ConstantsTemplate.swift
+++ YoutubeIosApplication/ConstantsTemplate.swift
@@ -1,14 +1,14 @@
-
-
-import Foundation
-
-struct Constants {
-    static var API_KEY = ""
-    static var PLAYLIST_ID = ""  // Replace with your actual Playlist ID
-    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
-        
-      static var VEDIOCELL_ID="VideoCell"
-    
-}
-
-
+//
+//
+//import Foundation
+//
+//struct Constants {
+//    static var API_KEY = ""
+//    static var PLAYLIST_ID = ""  // Replace with your actual Playlist ID
+//    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+//        
+//      static var VEDIOCELL_ID="VideoCell"
+//    
+//}
+//
+//
